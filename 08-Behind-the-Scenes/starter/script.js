22b9ee3216b162e41bef88dcdc8a430cac2b385f
whydoifly <netpreecher@gmail.com>
2022-06-27 15:07:24 +0300

    this keyword

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,43 +1,69 @@
 'use strict';
 
-function calcAge(birthYear) {
-    const age = 2022 - birthYear;
+// function calcAge(birthYear) {
+//     const age = 2022 - birthYear;
 
-    function printAge() {
-        let output = `${firstName}, you are ${age}, born in ${birthYear}.`
-        console.log(output);
+//     function printAge() {
+//         let output = `${firstName}, you are ${age}, born in ${birthYear}.`
+//         console.log(output);
     
 
-        if (birthYear >= 1981 && birthYear <= 1996) {
-            const str = `Oh, and you're a millenial, ${firstName}`;
-            console.log(str);
+//         if (birthYear >= 1981 && birthYear <= 1996) {
+//             const str = `Oh, and you're a millenial, ${firstName}`;
+//             console.log(str);
 
-            function add(a, b) {
-                return a + b
-            }
-        }
-    }
-    printAge();
+//             function add(a, b) {
+//                 return a + b
+//             }
+//         }
+//     }
+//     printAge();
 
-    return age;
+//     return age;
+// }
+
+// const firstName = 'Arkadii';
+// calcAge(1996);
+
+// console.log(me)
+// console.log(job)
+// console.log(year)
+
+// var me = 'Arkadii';
+// let job = 'programmer';
+// const year = 1996;
+// function addDec(a, b) {
+//     return a + b;
+// }
+
+// const addExpr = function (a, b) {
+//     return a + b;
+// }
+
+// const addArrow = (a, b) => a + b;
+
+
+// This keyword
+
+const calcAge = function(birthYear) {
+    console.log(2037 - birthYear);
+    console.log(this);
 }
-
-const firstName = 'Arkadii';
 calcAge(1996);
 
-console.log(me)
-console.log(job)
-console.log(year)
-
-var me = 'Arkadii';
-let job = 'programmer';
-const year = 1996;
-function addDec(a, b) {
-    return a + b;
+const arkadii = {
+    year: 1996,
+    calcAge: function () {
+        console.log(2022 - this.year);
+    }
 }
 
-const addExpr = function (a, b) {
-    return a + b;
+arkadii.calcAge();
+
+const matilda = {
+    year: 2017
 }
 
-const addArrow = (a, b) => a + b;
+matilda.calcAge = arkadii.calcAge;
+console.log(matilda);
+matilda.calcAge();
