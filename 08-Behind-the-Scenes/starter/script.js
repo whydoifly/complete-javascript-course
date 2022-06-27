171cabb15916b4a380d5351759665d4d4fd55b26
whydoifly <netpreecher@gmail.com>
2022-06-27 11:41:54 +0300

    Scoping in practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+function calcAge(birthYear) {
+    const age = 2022 - birthYear;
+
+    function printAge() {
+        let output = `${firstName}, you are ${age}, born in ${birthYear}.`
+        console.log(output);
+    
+
+        if (birthYear >= 1981 && birthYear <= 1996) {
+            const str = `Oh, and you're a millenial, ${firstName}`;
+            console.log(str);
+
+            function add(a, b) {
+                return a + b
+            }
+        }
+    }
+    printAge();
+
+    return age;
+}
+
+const firstName = 'Arkadii';
+calcAge(1996);
