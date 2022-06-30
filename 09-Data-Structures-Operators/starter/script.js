665efa5396e61f33b81be9d39e18dde970c6ea89
whydoifly <netpreecher@gmail.com>
2022-06-30 18:41:08 +0300

    Looping Arrays: The for-of loop

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -48,6 +48,15 @@ const restaurant = {
     },
   },
 };
+/////////////////
+// For-Of Loop //
+/////////////////
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+for (const item of menu) console.log(item);
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`);
+}
 
 //////////////////////
 // Short Circuiting //
