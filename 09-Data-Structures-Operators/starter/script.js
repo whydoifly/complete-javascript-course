498fc7dceb044e99e467a358c128e63fd75cdddb
whydoifly <netpreecher@gmail.com>
2022-06-30 09:03:58 +0300

    Short Circuiting

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -49,47 +49,59 @@ const restaurant = {
   },
 };
 
+
+//////////////////////
+// Short Circuiting //
+//////////////////////
+
+restaurant.numGuests = 23;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
 /////////////////////////////////
 // Rest pattern and parameters //
 /////////////////////////////////
 
 // SPREAD, because of right side of =
-const arr = [1, 2, ...[3, 4]];
-// REST, because of left side of =
-const [a, b, ...others] = [1, 2, 3, 4, 5];
-console.log(a, b, others);
+// const arr = [1, 2, ...[3, 4]];
+// // REST, because of left side of =
+// const [a, b, ...others] = [1, 2, 3, 4, 5];
+// console.log(a, b, others);
 
-const [pizza, , risotto, ...otherFood] = [
-  ...restaurant.mainMenu,
-  ...restaurant.starterMenu,
-];
-console.log(pizza, risotto, otherFood);
+// const [pizza, , risotto, ...otherFood] = [
+//   ...restaurant.mainMenu,
+//   ...restaurant.starterMenu,
+// ];
+// console.log(pizza, risotto, otherFood);
 
 // Spread with objects
 
-const { sat, ...weekdays } = restaurant.openingHours;
-console.log(weekdays);
+// const { sat, ...weekdays } = restaurant.openingHours;
+// console.log(weekdays);
 
-// Spread with functions
+// // Spread with functions
 
-const add = function (...numbers) {
-  let sum = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    sum += numbers[i];
-  }
-  console.log(sum);
-};
+// const add = function (...numbers) {
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+//   }
+//   console.log(sum);
+// };
 
-add(2, 3);
-add(5, 3, 7, 9);
-add(5, 3, 7, 9, 11, 15, 21);
+// add(2, 3);
+// add(5, 3, 7, 9);
+// add(5, 3, 7, 9, 11, 15, 21);
 
 
-const x = [23, 5, 7];
-add(...x);
+// const x = [23, 5, 7];
+// add(...x);
 
-restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
-restaurant.orderPizza('mushrooms');
+// restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+// restaurant.orderPizza('mushrooms');
 
 // restaurant.orderDelivery({
 //   time: '22:30',
