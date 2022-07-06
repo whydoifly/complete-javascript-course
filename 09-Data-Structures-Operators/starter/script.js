e397b936b9e7028c92582562da0eb5926ee1ed4c
whydoifly <netpreecher@gmail.com>
2022-07-06 14:49:13 +0300

    Maps: Iteration

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -48,28 +48,55 @@ const restaurant = {
   openingHours,
 };
 
+/////////////////////
+// Maps: Iteration //
+/////////////////////
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct!'],
+  [false, 'Try again'],
+]);
+console.log(question);
+
+const hoursMap = new Map(Object.entries(openingHours));
+
+console.log(hoursMap);
+
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key} ${value}`);
+}
+
+const answer = Number(prompt('Your answer'));
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
 //////////
 // Maps //
 //////////
-const rest = new Map();
-rest.set('name', 'Classico Italiano');
-rest.set(1, 'Firenze Italy');
-rest.set(2, 'Lisbon, Portugal');
-rest
-  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
-  .set('open', 11)
-  .set('close', 23)
-  .set(true, 'We are open')
-  .set(false, 'We are closed');
-console.log(rest.get('name'));
-console.log(rest.get(true));
+// const rest = new Map();
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Firenze Italy');
+// rest.set(2, 'Lisbon, Portugal');
+// rest
+//   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//   .set('open', 11)
+//   .set('close', 23)
+//   .set(true, 'We are open')
+//   .set(false, 'We are closed');
+// console.log(rest.get('name'));
+// console.log(rest.get(true));
 
-const time = 21;
-console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
-console.log(rest.has('categories'));
-rest.delete(2);
-console.log(rest);
-// rest.clear();
+// const time = 21;
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+// console.log(rest.has('categories'));
+// rest.delete(2);
+// console.log(rest);
+// // rest.clear();
 
 //////////
 // Sets //
