12c55252b1bd24a755d77c86ba4e528ba3bbc4f7
whydoifly <netpreecher@gmail.com>
2022-07-06 13:45:02 +0300

    Maps: Fundamentals

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -49,37 +49,60 @@ const restaurant = {
 };
 
 //////////
+// Maps //
+//////////
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze Italy');
+rest.set(2, 'Lisbon, Portugal');
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, 'We are closed');
+console.log(rest.get('name'));
+console.log(rest.get(true));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+console.log(rest.has('categories'));
+rest.delete(2);
+console.log(rest);
+// rest.clear();
+
+//////////
 // Sets //
 //////////
-const ordersSet = new Set([
-  'Pasta',
-  'Pizza',
-  'Pizza',
-  'Risotto',
-  'Pasta',
-  'Pizza',
-]);
-console.log(ordersSet);
-console.log(new Set('Arkadii'));
-console.log(ordersSet.size);
-console.log(ordersSet.has('Pizza'));
-console.log(ordersSet.has('Bread'));
-ordersSet.add('Garlic Bread', 'Garlic Bread');
-console.log(ordersSet);
-ordersSet.delete('Risotto');
-console.log(ordersSet);
-// ordersSet.clear();
-console.log(ordersSet);
+// const ordersSet = new Set([
+//   'Pasta',
+//   'Pizza',
+//   'Pizza',
+//   'Risotto',
+//   'Pasta',
+//   'Pizza',
+// ]);
+// console.log(ordersSet);
+// console.log(new Set('Arkadii'));
+// console.log(ordersSet.size);
+// console.log(ordersSet.has('Pizza'));
+// console.log(ordersSet.has('Bread'));
+// ordersSet.add('Garlic Bread', 'Garlic Bread');
+// console.log(ordersSet);
+// ordersSet.delete('Risotto');
+// console.log(ordersSet);
+// // ordersSet.clear();
+// console.log(ordersSet);
 
-for (let order of ordersSet) {
-  console.log(order);
-}
+// for (let order of ordersSet) {
+//   console.log(order);
+// }
 
-// Example using sets
-const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
-const newStaff = [...new Set(staff)];
-console.log(newStaff);
-console.log()
+// // Example using sets
+// const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+// const newStaff = [...new Set(staff)];
+// console.log(newStaff);
+// console.log()
 
 ////////////////////
 // Looping Object //
