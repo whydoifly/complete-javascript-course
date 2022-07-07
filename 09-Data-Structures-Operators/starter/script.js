b1f6cd1e336196264763ccf58cb2af13f8affe08
whydoifly <netpreecher@gmail.com>
2022-07-07 09:39:18 +0300

    Working With Strings - Part 2

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -49,38 +49,102 @@ const restaurant = {
 };
 
 /////////////////////////////////
+// Working with strings Part 3 //
+/////////////////////////////////
+
+/////////////////////////////////
+// Working with strings Part 2 //
+/////////////////////////////////
+// const airline = 'TAP Air Portugal';
+
+// console.log(airline.toLocaleLowerCase());
+// console.log(airline.toUpperCase());
+
+// // Fix capitalization in name
+// const passenger = 'arKaDii';
+// const passengerLower = passenger.toLowerCase();
+// const passengerCorrect =
+//   passengerLower[0].toUpperCase() + passengerLower.slice(1);
+// console.log(passengerCorrect);
+
+// // Comparing emails
+// const email = 'login@arkadii.io';
+// const loginEmail = ' login@ARkadii.Io \n';
+
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowerEmail.trim();
+
+// const normalizedEmail = loginEmail.toLowerCase().trim();
+
+// console.log(email === normalizedEmail);
+
+// // Replacing
+// const priceRUS = '288,87₽';
+// const priceUS = priceRUS.replace('₽', '$').replace(',', '.');
+// console.log(priceUS);
+
+// const announcement =
+//   'All passengers come to boarding door 23. Boarding door 23!';
+
+// console.log(announcement.replace('door', 'gate'));
+// console.log(announcement.replace(/door/g, 'gate'));
+
+// // Booleans
+// const plane = 'Airbus A320neo';
+// console.log(plane.includes('A320'));
+// console.log(plane.includes('Boeing'));
+// console.log(plane.startsWith('Air'));
+
+// if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+//   console.log('Part of new airbus family');
+// }
+
+// // Practice exercise
+// const checkBaggage = function (items) {
+//   const baggage = items.toLowerCase();
+//   if (baggage.includes('knife') || baggage.includes('gun')) {
+//     console.log('You are not allowed on board');
+//   } else {
+//     console.log('Welcome aboard');
+//   }
+// };
+// checkBaggage('I have a laptop, some food and a pocket Knife');
+// checkBaggage('Socks and camera');
+// checkBaggage('Got some snacks and a gun for protection');
+
+/////////////////////////////////
 // Working with strings Part 1 //
 /////////////////////////////////
-const airline = 'Tap Air Portugal';
-const plane = 'Boeing 777';
-console.log(plane[0]);
-console.log(plane[1]);
-console.log('B737'[0]);
-console.log(airline.indexOf('r'));
-console.log(airline.indexOf('portugal'));
-console.log(airline.slice(4)); // returns new string
-console.log(airline.slice(4, 7));
-console.log(airline.slice(0, airline.indexOf(' ')));
-console.log(airline.slice(airline.lastIndexOf(' ') + 1));
-console.log(airline.slice(-2));
-console.log(airline.slice(1, -1));
+// const airline = 'Tap Air Portugal';
+// const plane = 'Boeing 777';
+// console.log(plane[0]);
+// console.log(plane[1]);
+// console.log('B737'[0]);
+// console.log(airline.indexOf('r'));
+// console.log(airline.indexOf('portugal'));
+// console.log(airline.slice(4)); // returns new string
+// console.log(airline.slice(4, 7));
+// console.log(airline.slice(0, airline.indexOf(' ')));
+// console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+// console.log(airline.slice(-2));
+// console.log(airline.slice(1, -1));
 
-const checkMiddleSeat = function (seat) {
-  // B and E are middle seats
-  const s = seat.slice(-1);
-  if (s === 'B' || s === 'E') {
-    console.log('You got the middle seat');
-  } else {
-    console.log('You got lucky');
-  }
-};
+// const checkMiddleSeat = function (seat) {
+//   // B and E are middle seats
+//   const s = seat.slice(-1);
+//   if (s === 'B' || s === 'E') {
+//     console.log('You got the middle seat');
+//   } else {
+//     console.log('You got lucky');
+//   }
+// };
 
-checkMiddleSeat('11B');
-checkMiddleSeat('23C');
-checkMiddleSeat('3E');
+// checkMiddleSeat('11B');
+// checkMiddleSeat('23C');
+// checkMiddleSeat('3E');
 
-console.log(new String('Aarkadii'));
-console.log(typeof new String('Aarkadii'));
+// console.log(new String('Aarkadii'));
+// console.log(typeof new String('Aarkadii'));
 
 /////////////////////
 // Maps: Iteration //
