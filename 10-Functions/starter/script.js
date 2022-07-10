140756d9158d6e1bdd1914f5a4f9ef491f7285cd
whydoifly <netpreecher@gmail.com>
2022-07-10 12:24:30 +0300

    How Passing Arguments Works: Value vs Reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,5 +1,40 @@
 'use strict';
 
+/////////////////////////////////////////////////////
+// How Passing Arguments Works: Value vs Reference //
+/////////////////////////////////////////////////////
+// const flight = 'LH234';
+// const arkadii = {
+//   name: 'Arkadii Cheprasov',
+//   passport: 2017025099,
+// };
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr. ' + passenger.name;
+
+//   if (passenger.passport === 2017025099) {
+//     alert('Check in');
+//   } else {
+//     alert('Wrong passport');
+//   }
+// };
+
+// checkIn(flight, arkadii);
+// console.log(flight);
+// console.log(arkadii);
+
+// Is the same as doing:
+// const flightNum = flight;
+// const passenger = arkadii;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000000);
+};
+
+newPassport(arkadii);
+checkIn(flight, arkadii);
+
 /////////////////////////
 // Default Paramteters //
 /////////////////////////
