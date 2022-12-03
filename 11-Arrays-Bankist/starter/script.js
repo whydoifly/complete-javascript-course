07668bcd3e62c45ab3056891b0c1fc9c3004a440
whydoifly <netpreecher@gmail.com>
2022-12-03 15:02:18 -0300

    forEach method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -108,3 +108,22 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // console.log(arr[arr.length - 1]);
 // console.log(arr.slice(-1)[0]);
 // console.log(arr.at(-1));
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const [i, movement] of movements.entries()) {
+//   if (movement > 0) {
+//     console.log(`Movement ${i + 1}: You deposited ${movement}`)
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`)
+//   }
+// }
+
+// console.log('---FOREACH---');
+// movements.forEach(function(mov, i, arr) {
+//   if (mov > 0) {
+//     console.log(`Movement ${i + 1}: You deposited ${mov}`)
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`)
+//   }
+// })
