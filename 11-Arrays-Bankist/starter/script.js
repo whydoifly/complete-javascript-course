3dd9cacf45b1de730008128b89962a65be47f2bd
whydoifly <netpreecher@gmail.com>
2022-12-13 19:27:05 -0300

    Coding Challenge #2 (Working with Arrays)

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -81,6 +81,25 @@ const displayMovements = function (movements) {
 
 displayMovements(account1.movements);
 
+const calcDisplayBalance = function(movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+
+calcDisplayBalance(account1.movements);
+
+const createUserNames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+createUserNames(accounts);
+
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -182,16 +201,6 @@ const currencies = new Map([
 
 // console.log(movementsDescriptions);
 
-// const createUserNames = function (accs) {
-//   accs.forEach(function (acc) {
-//     acc.username = acc.owner
-//       .toLowerCase()
-//       .split(' ')
-//       .map(name => name[0])
-//       .join('');
-//   });
-// };
-
 // Filter Method
 // const deposits = movements.filter(function(mov) {
 //   return mov > 0
@@ -212,8 +221,29 @@ const currencies = new Map([
 
 // console.log(withdrawals);
 
-// createUserNames(accounts);
-// console.log(accounts);
+// Reduce Method
+// const balance = movements.reduce((acc, cur) => acc + cur, 100);
+
+// console.log(balance);
+
+// let balance2 = 0;
+// for (const mov of movements) {
+//   balance2 += mov;
+// }
+// console.log(balance2);
+
+// // createUserNames(accounts);
+// // console.log(accounts);
+
+// // Maximum Value
+// const max = movements.reduce((acc, mov) => {
+//   if (acc > mov) {
+//     return acc;
+//   } else {
+//     return mov;
+//   }
+// }, movements[0]);
+// console.log(max);
 
 ///////////////////////////////////////
 // Coding Challenge #1
@@ -247,3 +277,35 @@ GOOD LUCK 😀
 // checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 // console.log('----------')
 // checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+// function calcAverageHumanAge(ages) {
+//   let humanAge;
+//   const humanAges = ages.map(dogAge => dogAge <= 2 ? humanAge = 2 * dogAge : humanAge = 16 + dogAge * 4);
+//   const adultAges = humanAges.filter(age => age >= 18);
+//   const ageAverage = adultAges.reduce((acc, age) => acc + age, 0) / adultAges.length;
+//   return ageAverage;
+// }
+
+// const age1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+// const age2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+// console.log(age1, age2);
