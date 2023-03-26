5f290ce813a39f61f9231208bd7379357d3726bb
whydoifly <netpreecher@gmail.com>
2023-03-26 11:41:11 -0300

    Constructor functions and 'new' operator

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never do this
+//   this.calcAge = function () {
+//     console.log(2037 - this.birthYear);
+//   };
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+console.log(jonas instanceof Person);
