b1465faba1983a580ce8ba2f9650b0555d83a1a5
whydoifly <netpreecher@gmail.com>
2023-03-27 15:14:39 -0300

    Setters and Getters

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -121,8 +121,8 @@ GOOD LUCK 😀
 
 // class declaration
 class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
     this.birthYear = birthYear;
   }
 
@@ -131,14 +131,29 @@ class PersonCl {
     console.log(2037 - this.birthYear);
   }
 
-    greet() {
+  greet() {
     console.log(`Hey ${this.firstName}`);
-    }
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
 }
 
-const jessica = new PersonCl('Jessica', 1996);
+const jessica = new PersonCl('Jessica Davis', 1996);
 console.log(jessica);
 jessica.calcAge();
+console.log(jessica.age);
 
 console.log(jessica.__proto__ === PersonCl.prototype);
 
@@ -147,3 +162,24 @@ console.log(jessica.__proto__ === PersonCl.prototype);
 // }
 
 jessica.greet();
+
+// Getters and Setters
+const walter = new PersonCl('Walter White', 1965);
+
+const account = {
+  owner: 'Jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
