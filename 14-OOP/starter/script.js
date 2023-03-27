e8d7dcedabc28223b0818bea1f85c8ce9ee77aef
whydoifly <netpreecher@gmail.com>
2023-03-27 13:16:06 -0300

    Prototypal Inheritance on Built-in objects

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -6,9 +6,9 @@ const Person = function (firstName, birthYear) {
   this.birthYear = birthYear;
 
   // Never do this
-//   this.calcAge = function () {
-//     console.log(2037 - this.birthYear);
-//   };
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
 };
 
 const jonas = new Person('Jonas', 1991);
@@ -22,8 +22,8 @@ console.log(jonas instanceof Person);
 // Prototypes
 console.log(Person.prototype);
 Person.prototype.calcAge = function () {
-    console.log(2037 - this.birthYear);
-}
+  console.log(2037 - this.birthYear);
+};
 
 jonas.calcAge();
 matilda.calcAge();
@@ -37,4 +37,30 @@ console.log(Person.prototype.isPrototypeOf(Person));
 // .prototypeOfLinkedObjects
 
 Person.prototype.species = 'Homo Sapiens';
-console.log(jonas.species, matilda.species);   
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
+
+console.log(jonas.__proto__);
+// Object.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 4, 5, 6, 9, 9]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
+
